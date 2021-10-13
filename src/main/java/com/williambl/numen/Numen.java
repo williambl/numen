f14a2eb19524412f396482accd69a4c00fde5106
williambl <william@williambl.com>
2021-10-13 16:23:53 +0100

    Initial Commit

---
+++ src/main/java/com/williambl/numen/Numen.java
@@ -0,0 +1,10 @@
+package com.williambl.numen;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Numen implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
