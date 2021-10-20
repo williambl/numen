068fe557a3948aabb54873a2861089aedb8e75cb
williambl <william@williambl.com>
2021-10-20 21:40:41 +0100

    Make crop growing spell interact with favour

--- src/main/kotlin/com/williambl/numen/spells/CropGrowingSpell.kt
+++ src/main/kotlin/com/williambl/numen/spells/CropGrowingSpell.kt
@@ -3,6 +3,8 @@ package com.williambl.numen.spells
 import com.williambl.numen.gods.AgriculturalGod
 import com.williambl.numen.gods.God
 import com.williambl.numen.gods.Gods
+import com.williambl.numen.gods.Gods.getFavour
+import com.williambl.numen.gods.Gods.modifyFavour
 import com.williambl.numen.spells.Spells.attachSpell
 import com.williambl.numen.spells.Spells.removeSpell
 import net.minecraft.entity.LivingEntity
@@ -10,6 +12,7 @@ import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.world.World
+import kotlin.math.min
 
 object CropGrowingSpell: Spell() {
     override val relevantGods: List<God> = listOf(Gods.AGRICULTURAL)
@@ -23,7 +26,11 @@ object CropGrowingSpell: Spell() {
     }
 
     override fun run(world: World, player: PlayerEntity, data: NbtCompound) {
-        player.attachSpell(this, NbtCompound().apply { putInt("TicksRemaining", 200) })
+        player.attachSpell(this, NbtCompound().apply {
+            putInt("TicksRemaining", 200)
+            putDouble("Favour", player.getFavour(Gods.AGRICULTURAL))
+        })
+        relevantGods.forEach { player.modifyFavour(it) { i -> i - min(i*0.3, 3.0) } }
     }
 
     override fun onTick(attachedTo: LivingEntity, data: NbtCompound) {
@@ -40,7 +47,7 @@ object CropGrowingSpell: Spell() {
             return
         }
 
-        AgriculturalGod.fertiliseAroundPlayer(attachedTo, 10, 3, 0.05)
+        AgriculturalGod.fertiliseAroundPlayer(attachedTo, 2+min(data.getDouble("Favour").toInt()*5, 10), 3+min(data.getDouble("Favour").toInt(), 4), 0.05)
         data.putInt("TicksRemaining", data.getInt("TicksRemaining") - 1)
     }
 }
