c6a28aeb6294442dd83db4668ab3fc4b14563a16
williambl <william@williambl.com>
2021-10-20 13:02:49 +0100

    Add textures

--- src/main/kotlin/com/williambl/numen/client/NumenClient.kt
+++ src/main/kotlin/com/williambl/numen/client/NumenClient.kt
@@ -4,6 +4,8 @@ import com.williambl.numen.client.spells.tablet.EditClayTabletGui
 import com.williambl.numen.id
 import com.williambl.numen.spells.Spells
 import com.williambl.numen.spells.tablet.EditClayTabletGuiDescription
+import com.williambl.numen.spells.tablet.getTabletText
+import net.fabricmc.fabric.api.`object`.builder.v1.client.model.FabricModelPredicateProviderRegistry
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry
 import net.minecraft.entity.player.PlayerInventory
@@ -23,6 +25,16 @@ fun clientInit() {
         val stack = buf.readItemStack()
         client.execute {
             client.gameRenderer.showFloatingItem(stack)
+        }
+    }
+
+    FabricModelPredicateProviderRegistry.register(Spells.FIRED_TABLET, id("lines_written")) { stack, _, _, _ ->
+        val text = stack.getTabletText()
+        when {
+            text.length > 50 -> 3.0f
+            text.length > 20 -> 2.0f
+            text.isNotEmpty() -> 1.0f
+            else -> 0.0f
         }
     }
 }
--- src/main/kotlin/com/williambl/numen/spells/tablet/infusion/InfusionCauldronBlock.kt
+++ src/main/kotlin/com/williambl/numen/spells/tablet/infusion/InfusionCauldronBlock.kt
@@ -2,9 +2,7 @@ package com.williambl.numen.spells.tablet.infusion
 
 import com.williambl.numen.id
 import com.williambl.numen.mixin.AbstractCauldronBlockAccessor
-import com.williambl.numen.mixin.LeveledCauldronBlockMixin
 import com.williambl.numen.spells.Spells
-import com.williambl.numen.spells.Spells.INFUSION_CAULDRON_BLOCK
 import com.williambl.numen.spells.tablet.readInfusions
 import com.williambl.numen.spells.tablet.setTabletInfusions
 import com.williambl.numen.spells.tablet.write
@@ -26,7 +24,6 @@ import net.minecraft.util.ActionResult
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
-import java.util.*
 
 val baseBehaviourMap: Map<Item, CauldronBehavior> = mapOf(
     Pair(Spells.WRITABLE_TABLET, CauldronBehavior { state, world, pos, player, hand, stack ->
@@ -52,7 +49,6 @@ object InfusionCauldronBlock: LeveledCauldronBlock(
     override fun onEntityCollision(state: BlockState, world: World, pos: BlockPos, entity: Entity) {
         super.onEntityCollision(state, world, pos, entity)
         if (entity is ItemEntity) {
-
             if (ItemTags.FLOWERS.contains(entity.stack.item) && isEntityTouchingFluid(state, pos, entity)) {
                 (world.getBlockEntity(pos) as InfusionCauldronBlockEntity).infusions.compute(entity.stack.item) { _, i ->
                     (i ?: 0) + 1
