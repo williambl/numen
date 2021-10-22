037e97fb9035502ea2755f0d6cf872a14a3a7b28
williambl <william@williambl.com>
2021-10-22 20:43:01 +0100

    Tweak clay tablet gui textbox positioning

--- src/main/kotlin/com/williambl/numen/spells/tablet/EditClayTabletGuiDescription.kt
+++ src/main/kotlin/com/williambl/numen/spells/tablet/EditClayTabletGuiDescription.kt
@@ -2,14 +2,10 @@ package com.williambl.numen.spells.tablet
 
 import com.williambl.numen.spells.Spells
 import io.github.cottonmc.cotton.gui.SyncedGuiDescription
-import io.github.cottonmc.cotton.gui.widget.WGridPanel
 import io.github.cottonmc.cotton.gui.widget.WPlainPanel
 import io.github.cottonmc.cotton.gui.widget.WTextField
 import io.github.cottonmc.cotton.gui.widget.data.Insets
 import net.minecraft.entity.player.PlayerInventory
-import net.minecraft.inventory.Inventory
-import net.minecraft.screen.PropertyDelegate
-import net.minecraft.screen.ScreenHandlerType
 
 
 class EditClayTabletGuiDescription(
@@ -27,6 +23,6 @@ class EditClayTabletGuiDescription(
 
         text = WTextField()
         text.maxLength = 256
-        root.add(text, 0, 0, 300, 200)
+        root.add(text, 0, 18, 300, 200)
     }
 }
